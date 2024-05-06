536b0d18d69ea0bc3de6520781e47801c717044f
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-05-06 16:42:40 -0500

    feat: :sparkles: Menu footer terminado con su animacion
    Se modifico la carga de la selecion del footer con su paginacion, recordar realizar los componentes separados para cada json

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-import { getAllRocketEngineTotal } from "./modules/rockets.js";
 import { 
     paginationRockets
 } from "./modulesComponents/pagination.js";
 
+let footerSelect = async(e, id)=>{
+    e.preventDefault();
+    let li = document.querySelectorAll(".footer ul li")
+    console.log(li);
+    for(let val of li){
+        let [a] = val.children
+        a.classList.remove('select');
+    }
+    let [a] = id.children
+    a.classList.add('select');   
+}
 
-let paginacion = document.querySelector("#paginacion");
-paginacion.innerHTML = ""
-paginacion.append(await paginationRockets())
 
+let rocket = document.querySelector("#rocket")
+rocket.addEventListener("click", async(e)=>{
+    await footerSelect(e, rocket)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationRockets())
+} )
+
+let capsules = document.querySelector("#capsules")
+capsules.addEventListener("click", async(e)=>{
+    await footerSelect(e, capsules)
+})
+
+
+rocket.click();
