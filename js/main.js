16b326101fa313db656b177e0367c5f77e73ed49
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-05-06 20:35:25 -0500

    feat: :sparkles: Paginacion terminada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { 
+    load,
+    paginationCapsules,
     paginationRockets
 } from "./modulesComponents/pagination.js";
 
 let footerSelect = async(e, id)=>{
     e.preventDefault();
+    await load();
     let li = document.querySelectorAll(".footer ul li")
-    console.log(li);
     for(let val of li){
         let [a] = val.children
         a.classList.remove('select');
@@ -26,7 +28,10 @@ rocket.addEventListener("click", async(e)=>{
 let capsules = document.querySelector("#capsules")
 capsules.addEventListener("click", async(e)=>{
     await footerSelect(e, capsules)
+    let paginacion = document.querySelector("#paginacion");
+    paginacion.innerHTML = ""
+    paginacion.append(await paginationCapsules())
 })
 
 
-rocket.click();
+capsules.click();
